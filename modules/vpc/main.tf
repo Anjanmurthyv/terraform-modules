eea587f1ba06fa9661a3154f0267922abd889554
Anjanmurthyv <154734125+Anjanmurthyv@users.noreply.github.com>
2024-01-27 15:14:24 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -2,15 +2,13 @@ provider "aws" {
   region = var.aws_region
 }
 
-# Creating a VPC!
-resource "aws_vpc" "test-vpc" {
-  
-  # IP Range for the VPC
-  cidr_block = "192.168.0.0/27"
-  
-  # Enabling automatic hostname assigning
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = var.vpc_cidr
+  enable_dns_support = true
   enable_dns_hostnames = true
+
   tags = {
-    Name = "test-vpc"
+    Name = "MyVPC"
   }
 }
