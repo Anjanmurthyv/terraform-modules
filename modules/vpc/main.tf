5af52e17c1144d5d860b1f7c1e26f47d4384ff02
Anjanmurthyv <154734125+Anjanmurthyv@users.noreply.github.com>
2024-01-27 13:09:11 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,1 +1,12 @@
-
+# Creating a VPC!
+resource "aws_vpc" "test-vpc" {
+  
+  # IP Range for the VPC
+  cidr_block = "192.168.0.0/27"
+  
+  # Enabling automatic hostname assigning
+  enable_dns_hostnames = true
+  tags = {
+    Name = "test-vpc"
+  }
+}
