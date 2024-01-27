822ea8b92d46c31c3b15f84ee77a0ab1979e6584
Anjanmurthyv <154734125+Anjanmurthyv@users.noreply.github.com>
2024-01-27 15:15:47 +0530

    Update output.tf

--- modules/vpc/output.tf
+++ modules/vpc/output.tf
@@ -1,3 +1,13 @@
 output "aws_region_output" {
   value = var.aws_region
 }
+
+output "vpc_id" {
+  description = "ID of the created VPC"
+  value       = aws_vpc.my_vpc.id
+}
+
+output "vpc_cidr_block" {
+  description = "CIDR block of the created VPC"
+  value       = aws_vpc.my_vpc.cidr_block
+}
