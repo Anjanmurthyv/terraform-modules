2c31ee7be0da0c9d0b833783183ae9308628a457
Anjanmurthyv <154734125+Anjanmurthyv@users.noreply.github.com>
2024-01-27 15:58:15 +0530

    Update variable.tf

--- modules/vpc/variable.tf
+++ modules/vpc/variable.tf
@@ -1,12 +1,9 @@
-variable "aws_region" {
-  description = "The AWS region to deploy resources to"
-  type        = string
-  default     = "ap-south-1" # Set your desired default region
-}
-
-
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
-  default     = "10.0.0.0/16"  # You can change this CIDR block to your desired value
+}
+
+variable "vpc_tags" {
+  description = "Tags for the VPC"
+  type        = map(string)
 }
