a9d2cdadc855b6ce9e08b63ea1033fc79c454427
Anjanmurthyv <154734125+Anjanmurthyv@users.noreply.github.com>
2024-02-19 21:36:15 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc" "my_vpc" {
-  cidr_block = var.vpc_cidr
-  enable_dns_support = true
-  enable_dns_hostnames = true
-  tags = var.vpc_tags
+  cidr_block           = var.cidr_block
+  enable_dns_support   = var.enable_dns_support
+  enable_dns_hostnames = var.enable_dns_hostnames
+  tags                 = var.tags
 }
