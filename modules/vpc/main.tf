7d0b75ff8ae37cfc633959815a4b439a436f8663
Anjanmurthyv <154734125+Anjanmurthyv@users.noreply.github.com>
2024-02-19 21:49:18 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -3,4 +3,8 @@ resource "aws_vpc" "my_vpc" {
   enable_dns_support   = var.enable_dns_support
   enable_dns_hostnames = var.enable_dns_hostnames
   tags                 = var.tags
+
+  providers = {
+    aws = aws.region_specific
+  }
 }
