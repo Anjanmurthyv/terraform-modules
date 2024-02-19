c8bd31fc3dd85482c07265c2f5acf17f96f3284a
Anjanmurthyv <154734125+Anjanmurthyv@users.noreply.github.com>
2024-02-19 21:36:28 +0530

    Update variable.tf

--- modules/vpc/variable.tf
+++ modules/vpc/variable.tf
@@ -1,9 +1,19 @@
-variable "vpc_cidr" {
+variable "cidr_block" {
   description = "CIDR block for the VPC"
-  type        = string
 }
 
-variable "vpc_tags" {
-  description = "Tags for the VPC"
+variable "enable_dns_support" {
+  description = "Enable DNS support for the VPC"
+  default     = true
+}
+
+variable "enable_dns_hostnames" {
+  description = "Enable DNS hostnames for the VPC"
+  default     = true
+}
+
+variable "tags" {
+  description = "Tags to apply to the VPC"
   type        = map(string)
+  default     = {}
 }
