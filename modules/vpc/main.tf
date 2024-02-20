3eb7c190e60a5404f3fd495f27fba85e81ab36a1
Anjanmurthyv <154734125+Anjanmurthyv@users.noreply.github.com>
2024-02-20 12:26:13 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,10 +1,64 @@
-provider "aws" {
-  region = var.region
+# modules/vpc/main.tf
+
+# Define a Terraform module to create a VPC in AWS
+
+variable "vpc_cidr_block" {
+  description = "The CIDR block for the VPC"
 }
 
-resource "aws_vpc" "my_vpc" {
-  cidr_block           = var.cidr_block
-  enable_dns_support   = var.enable_dns_support
-  enable_dns_hostnames = var.enable_dns_hostnames
-  tags                 = var.tags
+variable "subnet_cidr_blocks" {
+  type        = list(string)
+  description = "List of CIDR blocks for subnets"
+}
+
+variable "region" {
+  description = "AWS region"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr_block
+  enable_dns_support = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "MainVPC"
+  }
+}
+
+resource "aws_subnet" "public" {
+  count             = length(var.subnet_cidr_blocks)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.subnet_cidr_blocks[count.index]
+  availability_zone = "${var.region}a"
+
+  tags = {
+    Name = "PublicSubnet-${count.index}"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "MainVPC-Gateway"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "PublicRouteTable"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
 }
