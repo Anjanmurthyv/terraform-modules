0c315de2ac50b57d6c4513bc00e184c39d108235
Anjanmurthyv <154734125+Anjanmurthyv@users.noreply.github.com>
2024-02-20 14:54:00 +0530

    Update variable.tf

--- modules/vpc/variable.tf
+++ modules/vpc/variable.tf
@@ -1,1 +1,14 @@
+variable "region" {
+  description = "The AWS region"
+  type        = string
+}
 
+variable "vpc_cidr_block" {
+  description = "The CIDR block for the VPC"
+  type        = string
+}
+
+variable "vpc_name" {
+  description = "The name of the VPC"
+  type        = string
+}
